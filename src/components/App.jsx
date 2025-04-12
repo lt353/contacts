9d25e9f4f805061a36a149ea3fe09e094f9edfdd
Lindsay Trenton <lt353@hawaii.edu>
2025-04-12 11:37:51 -1000

    Added components to move from template to basic contacts project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,29 @@
 import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
-      <p>This is a simple template react app.</p>
-      <p>Feel free to modify it as you like.</p>
-      <p>Happy coding!</p>
+      <h1 className="heading">My Contacts</h1>
+      <Card
+        name={contacts[0].name}
+        imgURL={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        imgURL={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        imgURL={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
     </div>
   );
 }
