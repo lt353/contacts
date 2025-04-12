01fc2231137b4d117d65683f1cb64274381adac7
Lindsay Trenton <lt353@hawaii.edu>
2025-04-12 13:10:17 -1000

    got rid of ugly code by introducing mapping

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,16 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./Avatar";
 
+function createCard(contact) {
+  return <Card
+    key={contact.id} 
+    name={contact.name}
+    imgURL={contact.imgURL}
+    tel={contact.phone}
+    email={contact.email}
+  />;
+}
+
 function App() {
   return (
     <div>
@@ -11,24 +21,8 @@ function App() {
         imgURL="https://media.licdn.com/dms/image/v2/D4D03AQHn0_lQhXbvSg/profile-displayphoto-shrink_800_800/B4DZT8XcRbG8Ac-/0/1739400794507?e=1749686400&v=beta&t=VlWOJb46DirV3FXJ4_5p1J74spLaIKVv0siZoQYgUow" 
       />
 
-      <Card
-        name={contacts[0].name}
-        imgURL={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        imgURL={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        imgURL={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      />
+      {contacts.map(createCard)}
+
     </div>
   );
 }
