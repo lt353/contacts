ed9339c06a2b785dcf9be5c34fb6031dbc35c3dd
Lindsay Trenton <lt353@hawaii.edu>
2025-04-12 12:14:53 -1000

    Broke things down into smaller components and added an avatar of me

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <Avatar 
+        imgURL="https://media.licdn.com/dms/image/v2/D4D03AQHn0_lQhXbvSg/profile-displayphoto-shrink_800_800/B4DZT8XcRbG8Ac-/0/1739400794507?e=1749686400&v=beta&t=VlWOJb46DirV3FXJ4_5p1J74spLaIKVv0siZoQYgUow" 
+      />
+
       <Card
         name={contacts[0].name}
         imgURL={contacts[0].imgURL}
